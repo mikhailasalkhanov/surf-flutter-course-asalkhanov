52445348500d8cf9eeefae08588576563f1fe690
Mikhail Asalkhanov <mkhlslkhnv@gmail.com>
2021-02-26 18:52:03 +1000

    Text и TextStyle

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/// Виджет экрана "Список интересных мест"
 class SightListScreen extends StatefulWidget {
   @override
   _SightListScreenState createState() => _SightListScreenState();
@@ -11,7 +12,17 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBarTitle'), 
+        title: Text('Список\nинтересных мест',
+        style: TextStyle(
+          color: Color(0xFF3B3E5B),
+          fontFamily: 'Roboto',
+          fontSize: 32,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.bold,          
+        )),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 128,
       ),
     );
   }
