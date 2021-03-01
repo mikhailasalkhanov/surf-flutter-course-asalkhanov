d744cd76643b4818a387af138143a643170c0792
Mikhail Asalkhanov <mkhlslkhnv@gmail.com>
2021-03-01 18:19:38 +1000

    4.4.2
    RichText и TextSpan согласно дизайну

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -11,8 +11,43 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBarTitle'), 
-      ),
+        title: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'С', style: TextStyle(color: Color(0xff4CAF50)),
+                  ),
+                  TextSpan(
+                    text: 'писок\n'
+                  ),
+                ]
+              ),
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'и', style: TextStyle(color: Color(0xffFCDD3D)),
+                  ),
+                  TextSpan(
+                    text: 'нтересных мест'
+                  ),
+                ]
+              ),
+            ],
+            style: TextStyle(
+              color: Color(0xff3B3E5B),
+              fontFamily: 'Roboto',
+              fontSize: 32,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 128,
+      )
     );
   }
 }
