9620cc23fdb563aba30edc92cbeb8fa1d3ba46f8
Mikhail Asalkhanov <mkhlslkhnv@gmail.com>
2021-03-09 18:19:36 +1000

    Исправил текст аппбара(цвета первых букв)

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:places/ui/res/strings/strings.dart';
+import 'package:places/ui/res/text_styles.dart';
 
 class SightListScreen extends StatefulWidget {
   @override
@@ -11,38 +13,8 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: RichText(
-          text: TextSpan(
-            children: [
-              TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'С', style: TextStyle(color: Color(0xff4CAF50)),
-                  ),
-                  TextSpan(
-                    text: 'писок\n'
-                  ),
-                ]
-              ),
-              TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'и', style: TextStyle(color: Color(0xffFCDD3D)),
-                  ),
-                  TextSpan(
-                    text: 'нтересных мест'
-                  ),
-                ]
-              ),
-            ],
-            style: TextStyle(
-              color: Color(0xff3B3E5B),
-              fontFamily: 'Roboto',
-              fontSize: 32,
-              fontStyle: FontStyle.normal,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+        title: Text(sightListScreenAppBarText,
+          style: textStyle
         ),
         backgroundColor: Colors.transparent,
         elevation: 0,
