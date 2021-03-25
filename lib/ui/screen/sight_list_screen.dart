f6ef84192bc7462e8c69bc89d7cf75c7b109ced7
Mikhail Asalkhanov <mkhlslkhnv@gmail.com>
2021-03-25 23:20:35 +1000

    Вывод карточек на экран

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:places/domain/sight.dart';
+import 'package:places/mocks.dart';
+import 'package:places/ui/res/colors.dart';
 import 'package:places/ui/res/strings/strings.dart';
 import 'package:places/ui/res/text_styles.dart';
+import 'package:places/ui/screen/sight_card.dart';
 
+/// Экран "Список интересных мест"
 class SightListScreen extends StatefulWidget {
   @override
   _SightListScreenState createState() => _SightListScreenState();
@@ -12,14 +17,26 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(sightListScreenAppBarText,
-          style: textStyle
+      appBar: AppBar(        
+        title: const Padding(
+          padding: const EdgeInsets.only(top: 24),
+          child: const Text(
+            Strings.sightListAppBar,
+            style: const TextStyles.largeTitle(AppColors.secondary),          
+          ),
         ),
         backgroundColor: Colors.transparent,
         elevation: 0,
         toolbarHeight: 128,
-      )
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            children: sightMocks,
+          ),
+        ),
+      ),
     );
   }
 }
