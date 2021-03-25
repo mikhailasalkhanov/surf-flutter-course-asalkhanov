d409fa4ddd3f3f29d91ddc0e702dbb2c65abc1e2
Mikhail Asalkhanov <mkhlslkhnv@gmail.com>
2021-03-25 20:39:01 +1000

    Вынес переиспользуемые константы и стили в отдельные классы

--- lib/ui/res/colors.dart
+++ lib/ui/res/colors.dart
@@ -1,5 +1,34 @@
 import 'dart:ui';
+import 'package:flutter/material.dart';
 
-const Color textColorPrimary = _blueZodiac;
+/// Цветовые алиасы, используемые в приложении
+class AppColors {
+  /// Темно-синий
+  static const Color main = _darkBlue;
+  
+  /// Бело-серый
+  static const Color background = _whiteSmoke;
 
-const Color _blueZodiac = Color(0xFF3B3E5B);
+  /// Фиолетовый
+  static const Color secondary = _violet;
+  
+  /// Темно-серо-синий
+  static const Color secondary2 = _darkGrayishBlue;
+  
+  /// Темно-серо-синий полупрозрачный
+  static const Color inactiveBlack = _darkGrayishBlueHalfOpacity;
+  
+  /// Белый
+  static const Color white = const Color(0xffFFFFFF);
+
+  /// Цветовая палитра
+  static const Color _darkBlue = const Color(0xff252849);
+
+  static const Color _violet = const Color(0xff3B3E5B);
+
+  static const Color _whiteSmoke = const Color(0xffF5F5F5);
+
+  static const Color _darkGrayishBlue = const Color(0xff7C7E92);
+
+  static const Color _darkGrayishBlueHalfOpacity = const Color.fromRGBO(124, 126, 146, 0.56);
+}
--- lib/ui/res/text_styles.dart
+++ lib/ui/res/text_styles.dart
@@ -1,10 +1,69 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:places/ui/res/colors.dart';
 
-TextStyle textStyle = const TextStyle(
-  color: textColorPrimary, 
-  fontFamily: 'Roboto', 
-  fontSize: 32,
-  fontStyle: FontStyle.normal, 
-  fontWeight: FontWeight.bold,
-  );
+/// Используется для создания стилей с предустановленными параметрами и
+/// кастомным цветом [color].
+/// Следует использовать с "const" при использовании
+class TextStyles extends TextStyle {
+  /// Цвет текста
+  final Color color;
+
+  /// Шрифт, подключается из ассетов в pubspec.yaml
+  final String fontFamily = 'Roboto';
+  
+  /// Очень крупный жирный шрифт
+  const TextStyles.largeTitle(this.color)
+      : super(
+          fontSize: 32,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.bold,
+        );
+  
+  /// Крупный жирный шрифт
+  const TextStyles.title(this.color)
+      : super(
+          fontSize: 24,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.bold,
+        );
+
+  /// Средний нежирный шрифт
+  const TextStyles.subtitle(this.color)
+      : super(
+          fontSize: 18,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.w500,
+        );
+  
+  /// Обычный нежирный шрифт
+  const TextStyles.text(this.color)
+      : super(
+          fontSize: 16,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.w500,
+        );
+  
+  /// Мелкий жирный шрифт
+  const TextStyles.smallBold(this.color)
+      : super(
+          fontSize: 14,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.bold,
+        );
+  
+  /// Мелкий нежирный шрифт
+  const TextStyles.small(this.color)
+      : super(
+          fontSize: 14,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.w400,
+        );
+  
+  /// Мелкий жирный шрифт для кнопок
+  const TextStyles.button(this.color)
+      : super(
+          fontSize: 14,
+          fontStyle: FontStyle.normal,
+          fontWeight: FontWeight.bold,
+        );
+}
